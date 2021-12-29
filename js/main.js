19da60fe00887f337b8592f048a23a365b66d7af
PetyaWebDesign <petyawebdesign@gmail.com>
2021-12-29 17:07:24 +0200

    finished the desktop version

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
+// Main illustration 
 gsap.from('.animated-image', {
     scale: 0.5,
-    duration: 1
-})
+    duration: 1.5
+});
+
+// E-mail form 
+gsap.from('form', {
+    x: -200,
+    duration: 1.5
+});
+
+// Social icons
+const icons = [...document.querySelectorAll('.social-icon')];
+window.addEventListener('load', revealAnimation);
+
+function revealAnimation() {
+    const TLFADE = gsap.timeline();
+
+    TLFADE
+        .from(icons[0], { autoAlpha: 0, scale: 0.5 })
+        .from(icons[1], { autoAlpha: 0, scale: 0.5 })
+        .from(icons[2], { autoAlpha: 0, scale: 0.5 })
+        .from(icons[3], { autoAlpha: 0, scale: 0.5 })
+        .from(icons[4], { autoAlpha: 0, scale: 0.5 })
+        .from(icons[5], { autoAlpha: 0, scale: 0.5 })
+        .from(icons[6], { autoAlpha: 0, scale: 0.5 })
+}
